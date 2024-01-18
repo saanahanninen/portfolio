028dbe5fec9d04a5a0420994b636cb852181c760
Saana Hänninen <saana.hanninen@tuni.fi>
2024-01-18 20:48:53 +0200

    Updates

--- src/components/projects/Projects.jsx
+++
@@ -1,9 +0,0 @@
-import "./projects.scss";
-
-export default function Projects({ id, title, active, setSelected }) {
-    return (
-        <li className={active ? "projects active" : "projects"} onClick={() => setSelected(id)}>
-            {title}
-        </li>
-    );
-}
