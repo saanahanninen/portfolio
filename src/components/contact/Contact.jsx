804d3a866bbdb0149531ee5a8c634ea8a28fd980
Saana Hänninen <saana.hanninen@tuni.fi>
2024-10-18 11:00:31 +0300

    fix scrolling issues and enhance responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,7 +8,6 @@ export default function Contact() {
                 <img src={`${process.env.PUBLIC_URL}/assets/envelope.png`} alt="envelopes" />
             </div>
             <div className="right">
-                <h2>Contact</h2>
                 <a href="https://www.linkedin.com/in/saanahanninen/" target="_blank" rel="noreferrer" className="itemContainer">
                     <LinkedIn className="icon" />
                     <span>Saana Hänninen</span>
